68144cbf0f08863d5050d0e58e220d5606187aec
okubomasaya <ooookubo.48@gmail.com>
2021-07-28 15:48:12 +0000

    save

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!, without: [:index]
+  before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def index
